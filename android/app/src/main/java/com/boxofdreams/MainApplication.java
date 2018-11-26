5c7938e7c5190a188f9f8f92a2127c4b0af5448d
DESKTOP-PSILDHL\Amit <amit.dvirhanoch@gmail.com>
2018-11-26 10:15:46 +0200

    reamove storage module in react-native-firebase

--- android/app/src/main/java/com/boxofdreams/MainApplication.java
+++ android/app/src/main/java/com/boxofdreams/MainApplication.java
@@ -5,7 +5,6 @@ import android.app.Application;
 import com.facebook.react.ReactApplication;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
-import io.invertase.firebase.storage.RNFirebaseStoragePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -27,8 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new RNFirebasePackage(),
-            new RNFirebaseFirestorePackage(),
-            new RNFirebaseStoragePackage()
+            new RNFirebaseFirestorePackage()
       );
     }
 
